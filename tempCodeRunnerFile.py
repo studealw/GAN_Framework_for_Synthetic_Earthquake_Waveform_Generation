9b59198d4179e919604a6380593f050ed681aaa1
Jay <jaypranil1503@gmail.com>
2026-08-13 22:54:17 +0530

    stopped tracking an unnecesary file

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,4 @@
-            # nn.GroupNorm(1,128),
-            # nn.ReLU()  
+        # self.initial_dense = nn.Sequential(
+        # nn.Linear(self.config.latent_dim, self.config.channels * 25),
+        # nn.ReLU()
+        # )
